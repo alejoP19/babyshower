0378029f1f5eaaef712e7cac032fe4890c6c3d88
alejito <alejoecheverry33@gmail.com>
2025-05-24 14:36:13 -0500

    corrigiendo y alistando db 2

--- babyShowerTina/settings.py
+++ babyShowerTina/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = 'django-insecure-_n$n9a6s=t-voo@vi4^yic#lq^=5damr6fcwp(ovfnmr1$jb@j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['31.97.41.76','127.0.0.1','alejoideas.com','www.alejoideas.com']
+# ALLOWED_HOSTS = ['31.97.41.76','127.0.0.1','alejoideas.com','www.alejoideas.com']
+
+ALLOWED_HOSTS = ['31.97.41.76','alejoideas.com','www.alejoideas.com']
 
 
 # Application definition
