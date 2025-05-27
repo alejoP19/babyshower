b242536697861a7dd7ff52a7883861d4b86017b2
alejito <alejoecheverry33@gmail.com>
2025-05-27 14:46:22 -0500

    cambiando a debbug a true

--- babyShowerTina/settings.py
+++ babyShowerTina/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_n$n9a6s=t-voo@vi4^yic#lq^=5damr6fcwp(ovfnmr1$jb@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True
+DEBUG =  False
 
 # ALLOWED_HOSTS = ['31.97.41.76','127.0.0.1','alejoideas.com','www.alejoideas.com']
 
 # ALLOWED_HOSTS = ['31.97.41.76','alejoideas.com','www.alejoideas.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
